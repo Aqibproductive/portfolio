b491857f7ff2754d89e4dedad19bebdfd44e552f
AQIB <116457335+Aqibproductive@users.noreply.github.com>
2023-09-26 13:15:15 +0500

    add scroll up arrow

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
-// var typed = new typed(".text", {
-//     strings: ["Frontend Developer", "Web Developer"],
-//     typeSpeed: 100,
-//     backSpeed: 100,
-//     backDelay: 1000,
-//     loop: true
-// });
+// Get a reference to the button element
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// Add a scroll event listener to show/hide the button
+window.addEventListener('scroll', () => {
+    if (document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+// Add a click event listener to scroll to the top when the button is clicked
+scrollToTopBtn.addEventListener('click', () => {
+    // Scroll to the top of the document with a smooth behavior
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
